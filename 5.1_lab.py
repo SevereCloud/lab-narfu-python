874271aec7547232a9e8c077b1518b84f95f4899
Daniil Suvorov <severecloud@gmail.com>
2016-11-11 15:58:29 +0300

    Изменение работы

--- 5.1_lab.py
+++ 5.1_lab.py
@@ -47,35 +47,32 @@ class Heap:
 		#print (self.heap_list)
 		return l
 
-##    def __str__(self):
-##        if self.size == 0: return "<empty heap>"
-##        def recurse(h,i):
-##            if i > h.size: return [], 0, 0
-##            label = str(h.heap_list[i])
-##            left_lines, left_pos, left_width = recurse(h, i * 2)
-##            right_lines, right_pos, right_width = recurse(h, i * 2 + 1)
-##            middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
-##            pos = left_pos + middle // 2
-##            width = left_pos + middle + right_width - right_pos
-##            while len(left_lines) < len(right_lines):
-##                left_lines.append(' ' * left_width)
-##            while len(right_lines) < len(left_lines):
-##                right_lines.append(' ' * right_width)
-##            label = label.center(middle, '.')
-##            if label[0] == '.': label = ' ' + label[1:]
-##            if label[-1] == '.': label = label[:-1] + ' '
-##            lines = [' ' * left_pos + label + ' ' * (right_width - right_pos), ' ' * left_pos + '/' + ' ' * (middle-2) + '\\' + ' ' * (right_width - right_pos)] + \ [left_line + ' ' * (width - left_width - right_width) + right_line
-##            for left_line, right_line in zip(left_lines, right_lines)]
-##            return lines, pos, width
-##        return '\n'.join(recurse(self, 1) [0])*/
+	def __str__(self):
+		if self.size == 0: return "<empty heap>"
+		def recurse(h,i):
+			if i > h.size: return [], 0, 0
+			label = str(h.heap_list[i])
+			left_lines, left_pos, left_width = recurse(h, i * 2)
+			right_lines, right_pos, right_width = recurse(h, i * 2 + 1)
+			middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
+			pos = left_pos + middle // 2
+			width = left_pos + middle + right_width - right_pos
+			while len(left_lines) < len(right_lines):
+				left_lines.append(' ' * left_width)
+			while len(right_lines) < len(left_lines):
+				right_lines.append(' ' * right_width)
+			label = label.center(middle, '.')
+			if label[0] == '.': label = ' ' + label[1:]
+			if label[-1] == '.': label = label[:-1] + ' '
+			lines = [' ' * left_pos + label + ' ' * (right_width - right_pos), ' ' * left_pos + '/' + ' ' * (middle-2) + '\\' + ' ' * (right_width - right_pos)] + [left_line + ' ' * (width - left_width - right_width) + right_line
+			for left_line, right_line in zip(left_lines, right_lines)]
+			return lines, pos, width
+		return '\n'.join(recurse(self, 1) [0])
 
 h = Heap()
 h.build_heap([8, 4, 5, 7, 8, 6, 2])
+print (h)
+
 print(h.heap_list)
-print(h.extract_min())
-print(h.extract_min())
-print(h.extract_min())
-print(h.extract_min())
-print(h.extract_min())
-print(h.extract_min())
-print(h.extract_min())
+for i in range(h.size):
+	print(h.extract_min())
--- 5.2_lab.py
+++ 5.2_lab.py
@@ -1,3 +1,29 @@
+def strHeap(arr):
+	size = len(arr)
+	if size == 0: return "<empty heap>"
+	def recurse(i):
+		if i > h.size: return [], 0, 0
+		label = str(arr[i])
+		left_lines, left_pos, left_width = recurse( i * 2)
+		right_lines, right_pos, right_width = recurse( i * 2 + 1)
+		middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
+		pos = left_pos + middle // 2
+		width = left_pos + middle + right_width - right_pos
+		while len(left_lines) < len(right_lines):
+			left_lines.append(' ' * left_width)
+		while len(right_lines) < len(left_lines):
+			right_lines.append(' ' * right_width)
+		label = label.center(middle, '.')
+		if label[0] == '.': label = ' ' + label[1:]
+		if label[-1] == '.': label = label[:-1] + ' '
+		lines = [' ' * left_pos + label + ' ' * (right_width - right_pos), ' ' * left_pos + '/' + ' ' * (middle-2) + '\\' + ' ' * (right_width - right_pos)] + [left_line + ' ' * (width - left_width - right_width) + right_line
+		for left_line, right_line in zip(left_lines, right_lines)]
+		return lines, pos, width
+	return '\n'.join(recurse(1) [0])
+
+
+
+
 import time
 import random
 
@@ -36,48 +62,50 @@ class Heap:
 
 	def heap(self, l):
 		size = len(l)-1
+
 		for i in range(size, -1, -1):
 			if 2*i+1<=size:
 				if l[i]>l[2*i+1]:
 					l[i],l[2*i+1] = l[2*i+1], l[i]
 			if 2*i+2<=size:
 				if l[i]>l[2*i+2]:
-					l[i],l[2*i+2] = l[2*i+2], l[i] 
+					l[i],l[2*i+2] = l[2*i+2], l[i]
+	
 		return l
 
-##    def __str__(self):
-##        if self.size == 0: return "<empty heap>"
-##        def recurse(h,i):
-##            if i > h.size: return [], 0, 0
-##            label = str(h.heap_list[i])
-##            left_lines, left_pos, left_width = recurse(h, i * 2)
-##            right_lines, right_pos, right_width = recurse(h, i * 2 + 1)
-##            middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
-##            pos = left_pos + middle // 2
-##            width = left_pos + middle + right_width - right_pos
-##            while len(left_lines) < len(right_lines):
-##                left_lines.append(' ' * left_width)
-##            while len(right_lines) < len(left_lines):
-##                right_lines.append(' ' * right_width)
-##            label = label.center(middle, '.')
-##            if label[0] == '.': label = ' ' + label[1:]
-##            if label[-1] == '.': label = label[:-1] + ' '
-##            lines = [' ' * left_pos + label + ' ' * (right_width - right_pos), ' ' * left_pos + '/' + ' ' * (middle-2) + '\\' + ' ' * (right_width - right_pos)] + \ [left_line + ' ' * (width - left_width - right_width) + right_line
-##            for left_line, right_line in zip(left_lines, right_lines)]
-##            return lines, pos, width
-##        return '\n'.join(recurse(self, 1) [0])*/
 
-for n in [10,1000,2000,3000,4000,5000,10000]:
+	def __str__(self):
+		if self.size == 0: return "<empty heap>"
+		def recurse(h,i):
+			if i > h.size: return [], 0, 0
+			label = str(h.heap_list[i])
+			left_lines, left_pos, left_width = recurse(h, i * 2)
+			right_lines, right_pos, right_width = recurse(h, i * 2 + 1)
+			middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
+			pos = left_pos + middle // 2
+			width = left_pos + middle + right_width - right_pos
+			while len(left_lines) < len(right_lines):
+				left_lines.append(' ' * left_width)
+			while len(right_lines) < len(left_lines):
+				right_lines.append(' ' * right_width)
+			label = label.center(middle, '.')
+			if label[0] == '.': label = ' ' + label[1:]
+			if label[-1] == '.': label = label[:-1] + ' '
+			lines = [' ' * left_pos + label + ' ' * (right_width - right_pos), ' ' * left_pos + '/' + ' ' * (middle-2) + '\\' + ' ' * (right_width - right_pos)] + [left_line + ' ' * (width - left_width - right_width) + right_line
+			for left_line, right_line in zip(left_lines, right_lines)]
+			return lines, pos, width
+		return '\n'.join(recurse(self, 1) [0])
+
+s = 0
+while s == 0:
+	n = int(input ('\n size array '))
 	arr = []
 	for i in range(n): arr.append(random.randint(0, 100))
 
-	t1 =time.clock()
-
 	h = Heap()
 	h.build_heap(arr)
+	print (h)
 	for l in range(n):
-		h.extract_min()
-		
-	t2 = time.clock()
-	t = (t2 - t1)*1000
-	print "{0:7}:".format(n) + "{0:10.1f}".format(t)
+		print (h.extract_min())
+		print ()
+		print (h)
