bdc4f0d01b2b1d425bbd1f4ede7b502f41416c68
Daniil Suvorov <severecloud@gmail.com>
2016-11-20 14:50:45 +0300

    Отредактирован вывод

--- 4.1_lab.py
+++ 4.1_lab.py
@@ -41,23 +41,26 @@ class BinaryTree:
         self.root = obj 
 
 
-    def draw(self):
-        s = []
-        self.i = 0
-        self.ii = 0
-        def dis(obj):
-            s.append(str(obj.root))
-            t = (' '*self.i)+str(obj.root) +':\n'
-            self.i += 1
-            ii = self.i
-            if obj.left != None:
-                t +=(' '*self.i)+ dis(obj.left)
-                self.i = ii
-            if obj.right != None:
-                t +=(' '*self.i)+ dis(obj.right)
-            return t
-
-        return str(dis(self))
+    def __str__(self):
+        if self.root is None: return '<empty tree>'
+        def recurse(node):
+            if node is None: return [], 0, 0
+            label = str(node.root)
+            left_lines, left_pos, left_width = recurse(node.left)
+            right_lines, right_pos, right_width = recurse(node.right)
+            middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
+            pos = left_pos + middle // 2
+            width = left_pos + middle + right_width - right_pos
+            while len(left_lines) < len(right_lines):
+                left_lines.append(' ' * left_width)
+            while len(right_lines) < len(left_lines):
+                right_lines.append(' ' * right_width)
+            label = label.center(middle, '.')
+            if label[0] == '.': label = ' ' + label[1:]
+            if label[-1] == '.': label = label[:-1] + ' '
+            lines = [' ' * left_pos + label + ' ' * (right_width - right_pos), ' ' * left_pos + '/' + ' ' * (middle-2) + '\\' + ' ' * (right_width - right_pos)] + [left_line + ' ' * (width - left_width - right_width) + right_line for left_line, right_line in zip(left_lines, right_lines)]
+            return lines, pos, width
+        return '\n'.join(recurse(self) [0])
 
         
 
@@ -74,7 +77,8 @@ t.insertRight('right')
 print(t.getRightChild())
 print(t.getRightChild().getRoot())
 
-t.getRightChild().setRoot('right root')
-print(t.getRightChild().getRoot())
+t.getRightChild().insertLeft('right root')
 
-print(t.draw())
+
+print()
+print(t)
--- 4.4_lab.py
+++ 4.4_lab.py
@@ -37,25 +37,25 @@ class BinaryTree:
         self.root = obj
 
     def __str__(self):
-        self.i = 0
-        def dis(obj):
-            t =''
-            
-            ii = self.i
-
-            if obj.left != None:
-                self.i += 1
-                t += dis(obj.left)
-
-            t += ('.'*ii)+str(obj.root) +'\n'
-
-            self.i = ii
-            if obj.right != None:
-                self.i += 1
-                t +=dis(obj.right)
-            return t
-
-        return str(dis(self))
+        if self.root is None: return '<empty tree>'
+        def recurse(node):
+            if node is None: return [], 0, 0
+            label = str(node.root)
+            left_lines, left_pos, left_width = recurse(node.left)
+            right_lines, right_pos, right_width = recurse(node.right)
+            middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
+            pos = left_pos + middle // 2
+            width = left_pos + middle + right_width - right_pos
+            while len(left_lines) < len(right_lines):
+                left_lines.append(' ' * left_width)
+            while len(right_lines) < len(left_lines):
+                right_lines.append(' ' * right_width)
+            label = label.center(middle, '.')
+            if label[0] == '.': label = ' ' + label[1:]
+            if label[-1] == '.': label = label[:-1] + ' '
+            lines = [' ' * left_pos + label + ' ' * (right_width - right_pos), ' ' * left_pos + '/' + ' ' * (middle-2) + '\\' + ' ' * (right_width - right_pos)] + [left_line + ' ' * (width - left_width - right_width) + right_line for left_line, right_line in zip(left_lines, right_lines)]
+            return lines, pos, width
+        return '\n'.join(recurse(self) [0])
 
 
 def buildExprTree(s):
