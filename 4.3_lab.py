784735ea169e5213b21b993756e2e204ee63cac7
Даниил Суворов < >
2017-01-23 21:04:14 +0300

    Создание markdown файлов

--- 4.3_lab.py
+++ 4.3_lab.py
@@ -2,39 +2,39 @@ import operator
 
 
 class BinaryTree:
-	def __init__(self,rootNode):
-		self.root = rootNode
-		self.left = None
-		self.right = None
+    def __init__(self,rootNode):
+        self.root = rootNode
+        self.left = None
+        self.right = None
 
-	def insertLeft(self,newNode):
-		if self.left == None:
-			self.left = BinaryTree(newNode)
-		else:
-			t = BinaryTree(newNode)
-			t.left = self.left
-			self.left = t
+    def insertLeft(self,newNode):
+        if self.left == None:
+            self.left = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.left = self.left
+            self.left = t
 
-	def insertRight(self,newNode):
-		if self.right == None:
-			self.right = BinaryTree(newNode)
-		else:
-			t = BinaryTree(newNode)
-			t.right = self.right
-			self.right = t
+    def insertRight(self,newNode):
+        if self.right == None:
+            self.right = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.right = self.right
+            self.right = t
 
-	def getRoot(self):
-		return self.root
+    def getRoot(self):
+        return self.root
 
-	def getRightChild(self):
-		return self.right
+    def getRightChild(self):
+        return self.right
 
 
-	def getLeftChild(self):
-		return self.left
+    def getLeftChild(self):
+        return self.left
 
-	def setRoot(self,obj):
-		self.root = obj 
+    def setRoot(self,obj):
+        self.root = obj 
 
     def __str__(self):
         if self.root is None: return '<empty tree>'
@@ -59,59 +59,59 @@ class BinaryTree:
 
 
 def buildExprTree(s):
-	expr = s.split()
-	# Используем список stack в качестве стека для возврата к
-	# родителю текущего узла. Для этого используем методы списка
-	# append() и pop()
-	stack = []
-	tree = BinaryTree('')
-	stack.append(tree)
-	currentTree = tree
-	for i in expr:
-		if i == '(':
-			currentTree.insertLeft('')
-			stack.append(currentTree)
-			currentTree = currentTree.getLeftChild()
-		elif i not in ['+', '-', '*', '/', ')']:
-			currentTree.setRoot(i)
-			currentTree = stack.pop()
-		elif i in ['+', '-', '*', '/']:
-			currentTree.setRoot(i)
-			currentTree.insertRight('')
-			stack.append(currentTree)
-			currentTree = currentTree.getRightChild()
-		elif i == ')':
-			currentTree = stack.pop()
-		else:
-			raise ValueError
-	return tree
+    expr = s.split()
+    # Используем список stack в качестве стека для возврата к
+    # родителю текущего узла. Для этого используем методы списка
+    # append() и pop()
+    stack = []
+    tree = BinaryTree('')
+    stack.append(tree)
+    currentTree = tree
+    for i in expr:
+        if i == '(':
+            currentTree.insertLeft('')
+            stack.append(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in ['+', '-', '*', '/', ')']:
+            currentTree.setRoot(i)
+            currentTree = stack.pop()
+        elif i in ['+', '-', '*', '/']:
+            currentTree.setRoot(i)
+            currentTree.insertRight('')
+            stack.append(currentTree)
+            currentTree = currentTree.getRightChild()
+        elif i == ')':
+            currentTree = stack.pop()
+        else:
+            raise ValueError
+    return tree
 
 class evalExprTree:
-	def __init__(self,tree):
-		self.result = self.count(tree)
+    def __init__(self,tree):
+        self.result = self.count(tree)
 
-	def operation(self,o,l,r):
-		opers = {'+':operator.add, '-':operator.sub, '*':operator.mul,'/':operator.truediv}
-		f = opers[o]
-		return f(l,r)
+    def operation(self,o,l,r):
+        opers = {'+':operator.add, '-':operator.sub, '*':operator.mul,'/':operator.truediv}
+        f = opers[o]
+        return f(l,r)
 
 
-	def count(self, tree):
-		o = tree.root
-		try:
-			l = int(tree.getLeftChild().root)
-		except ValueError:
-			l = self.count(tree.getLeftChild())
+    def count(self, tree):
+        o = tree.root
+        try:
+            l = int(tree.getLeftChild().root)
+        except ValueError:
+            l = self.count(tree.getLeftChild())
 
-		try:
-			r = int(tree.getRightChild().root)
-		except ValueError:
-			r = self.count(tree.getRightChild())
-		
-		return self.operation(o,l,r)
+        try:
+            r = int(tree.getRightChild().root)
+        except ValueError:
+            r = self.count(tree.getRightChild())
+        
+        return self.operation(o,l,r)
 
-	def __str__(self):
-		return self.result
+    def __str__(self):
+        return self.result
 
 
 t = buildExprTree("( ( 9 + 3 ) * 6 )")
--- 5.1_lab.py
+++ 5.1_lab.py
@@ -1,6 +1,7 @@
 class Heap:
+
     def __init__(self):
-        self.heap_list= [0]
+        self.heap_list = [0]
         self.size = 0
 
     def extract_min(self):
@@ -18,39 +19,42 @@ class Heap:
             i = mc
         return r
 
-    def min_child(self,i):
+    def min_child(self, i):
         if i * 2 + 1 > self.size:
             return i * 2
         else:
-            if self.heap_list[i*2] < self.heap_list[i*2+1]:
+            if self.heap_list[i * 2] < self.heap_list[i * 2 + 1]:
                 return i * 2
             else:
                 return i * 2 + 1
 
     def build_heap(self, l):
         self.size = len(l)
-        self.heap_list+=self.heap(l)
+        self.heap_list += self.heap(l)
 
     def heap(self, l):
-        size = len(l)-1
+        size = len(l) - 1
         #print ("arr ", self.heap_list)
         for i in range(size, -1, -1):
             #print ("i ", l[i])
-            if 2*i+1<=size:
+            if 2 * i + 1 <= size:
                 #print ("x ",l[i],l[2*i])
-                if l[i]>l[2*i+1]:
-                    l[i],l[2*i+1] = l[2*i+1], l[i]
-            if 2*i+2<=size:
+                if l[i] > l[2 * i + 1]:
+                    l[i], l[2 * i + 1] = l[2 * i + 1], l[i]
+            if 2 * i + 2 <= size:
                 #print ("x ",l[i],l[2*i],l[2*i+1])
-                if l[i]>l[2*i+2]:
-                    l[i],l[2*i+2] = l[2*i+2], l[i] 
+                if l[i] > l[2 * i + 2]:
+                    l[i], l[2 * i + 2] = l[2 * i + 2], l[i]
         #print (self.heap_list)
         return l
 
     def __str__(self):
-        if self.size == 0: return "<empty heap>"
-        def recurse(h,i):
-            if i > h.size: return [], 0, 0
+        if self.size == 0:
+            return "<empty heap>"
+
+        def recurse(h, i):
+            if i > h.size:
+                return [], 0, 0
             label = str(h.heap_list[i])
             left_lines, left_pos, left_width = recurse(h, i * 2)
             right_lines, right_pos, right_width = recurse(h, i * 2 + 1)
@@ -62,16 +66,18 @@ class Heap:
             while len(right_lines) < len(left_lines):
                 right_lines.append(' ' * right_width)
             label = label.center(middle, '.')
-            if label[0] == '.': label = ' ' + label[1:]
-            if label[-1] == '.': label = label[:-1] + ' '
-            lines = [' ' * left_pos + label + ' ' * (right_width - right_pos), ' ' * left_pos + '/' + ' ' * (middle-2) + '\\' + ' ' * (right_width - right_pos)] + [left_line + ' ' * (width - left_width - right_width) + right_line
-            for left_line, right_line in zip(left_lines, right_lines)]
+            if label[0] == '.':
+                label = ' ' + label[1:]
+            if label[-1] == '.':
+                label = label[:-1] + ' '
+            lines = [' ' * left_pos + label + ' ' * (right_width - right_pos), ' ' * left_pos + '/' + ' ' * (middle - 2) + '\\' + ' ' * (right_width - right_pos)] + [left_line + ' ' * (width - left_width - right_width) + right_line
+                                                                                                                                                                      for left_line, right_line in zip(left_lines, right_lines)]
             return lines, pos, width
-        return '\n'.join(recurse(self, 1) [0])
+        return '\n'.join(recurse(self, 1)[0])
 
 h = Heap()
 h.build_heap([8, 4, 5, 7, 8, 6, 2])
-print (h)
+print(h)
 
 print(h.heap_list)
 for i in range(h.size):
